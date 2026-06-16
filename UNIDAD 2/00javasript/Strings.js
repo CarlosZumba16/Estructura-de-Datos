750f9986c0ab40edc958a96cde21d14445d6662e
Carlos Zumba <carlos.d.zumba@unl.edu.ec>
2026-06-16 11:35:56 -0500

    feat:Clase[16/06/2026][Carlos Zumba]

---
+++ UNIDAD 2/00javasript/Strings.js
@@ -0,0 +1,19 @@
+let string = 'Hola mundo'
+let string2 = "Javascript es chere!" // ¡Me gusta el toque ecuatoriano!
+let string3 = `${string} feliz :)`   // Corregido con comillas invertidas
+let string4 = string + ' ' + string2
+
+console.log(string)
+console.log(string2)
+console.log(strin3)
+console.log(string4)
+
+
+let frase ='Javascript es genila!'
+console.log(frase.length)
+console.log(frase.toLocaleLowerCase)
+console.log(frase.toLowerCase)
+console.log(frase.substring(0.10))
+
+
+
---
+++ UNIDAD 2/00javasript/operadores-comparacion.js
@@ -0,0 +1,21 @@
+/*
+== 
+=== el mismo tipo y del mismo valor
+===!=
+|=
+|==
+
+
+<
+>
+
+and
+
+or
+
+not
+los tipos de condicionales nvestigando y si se utiliza un bucle for se crea un hatml klamando al java script en un sitio web
+
+
+
+*/
