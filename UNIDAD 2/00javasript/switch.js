e0aaf261d49e1533eaef4cefc4f355265b6b8c75
Carlos Zumba <carlos.d.zumba@unl.edu.ec>
2026-06-17 11:17:56 -0500

    La clase de 17/06/2026 y la modificacion del html

---
+++ UNIDAD 2/00javasript/switch.js
@@ -0,0 +1,17 @@
+let epcr='mangos'
+switch (epcr) {
+    case "mangos":
+        console.log("SOy ese mero compa el mango")
+        break;
+    case "manzanas":
+        console.log("Compa yo no soy el mango soy el maestro manzanero")
+        break;
+    case "maranjas":
+        console.log("No bro soy naranja no mango")
+        break;
+
+    default:
+        console.log("Mis disculpas no encotre lo que me pediste")
+        break;
+}
+console.log('losiento no comtamos con dicho pedido')
