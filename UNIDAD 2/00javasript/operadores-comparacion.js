3dd47029a57bb2ce94d900aa3e3bfb82555de1af
Carlos Zumba <carlos.d.zumba@unl.edu.ec>
2026-06-17 10:23:11 -0500

    Las condicionales y html

--- UNIDAD 2/00javasript/operadores-comparacion.js
+++ UNIDAD 2/00javasript/operadores-comparacion.js
@@ -19,3 +19,18 @@ los tipos de condicionales nvestigando y si se utiliza un bucle for se crea un h
 
 
 */
+let stock = 0;
+let precioProducto = 25.50;
+let dineroDisponible = 50;
+
+// Ejemplo 1: Validar si hay mercancía usando conversión estricta
+if (stock === 0) {
+    console.log("Lo sentimos, producto agotado.");
+}
+
+// Ejemplo 2: Validar si te alcanza el dinero
+if (dineroDisponible >= precioProducto) {
+    console.log("¡Compra realizada con éxito!");
+} else {
+    console.log("Fondos insuficientes.");
+}
